91c2b660119db65d97684368647d62fbb57c8ace
JJoSuk <loveyou55042@gmail.com>
2023-07-25 14:36:10 +0900

    MannaM

---
+++ src/main/java/demo/mannam_project/MannaMProjectApplication.java
@@ -0,0 +1,13 @@
+package demo.mannam_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MannaMProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MannaMProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/demo/mannam_project/MannaMProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.mannam_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MannaMProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
