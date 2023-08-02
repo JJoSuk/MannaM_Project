855cb067df69196894f78bc4379936c0d169df33
ebyong <ush1111@naver.com>
2023-08-02 15:42:07 +0900

    delete post*

--- src/main/java/demo/mannam_project/repository/PostRepository.java
+++
@@ -1,8 +0,0 @@
-package demo.mannam_project.repository;
-
-import demo.mannam_project.domain.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Integer> {
-
-}
